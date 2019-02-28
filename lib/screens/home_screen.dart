dc77473c31ead44041ca8e495b11e26de216fbf3
daniel-lucas-silva <dluc18@gmail.com>
2019-02-27 21:44:50 -0300

    defining base structure

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
